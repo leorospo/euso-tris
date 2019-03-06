f817f1c1fdc6a97e7971c0fe7113761bd41ffe42
Leonardo Mancori <leonardo.mancori@gmail.com>
2019-03-06 17:42:02 +0100

    basic tris functionality - missing winning managment and reset game

--- tests/getWinner test.js
+++ tests/getWinner test.js
@@ -1,12 +1,25 @@
 
+describe("Winning options", function() {
 
-describe("getWinner test", function() {
-    it("behaves as it should", function() {
+    var initialState = [[,,],[,,],[,,]]
+    var crossWins1 = [[1,1,1],[,,],[,,]]
+    var crossWins1 = [[1,,],[1,,],[1,,]]
+    var crossWins1 = [[1,,],[,1,],[,,1]]
 
-
-
-      expect(getWinner()).toBe(true);
+    it("Cross wins", function() {
+        expect(getWinner()).toBe('cross');
     });
-  });
-  
-  
+
+    it("Circle wins", function() {
+        expect(getWinner()).toBe('circle');
+    });
+
+    it("Draw", function() {
+        expect(getWinner()).toBe('draw');
+    });
+
+    it("Not ending move", function() {
+        expect(getWinner(initialState)).toBe('null');
+    });
+
+});
